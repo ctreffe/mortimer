ff339b7fb9d152dd4430711373bd986dbb6cce90
Christian Treffenstädt <treffenstaedt@psych.uni-goettingen.de>
2017-01-17 12:06:04 +0100

    Update forms.py
    Change deprecated code

--- mortimer/forms.py
+++ mortimer/forms.py
@@ -1,32 +1,32 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, BooleanField, TextAreaField, \
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, \
     SelectField, FileField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    username = TextField('Name')
+class LoginForm(FlaskForm):
+    username = StringField('Name')
     password = PasswordField('Password')
 
-class ExperimentForm(Form):
-    name = TextField('Name')
+class ExperimentForm(FlaskForm):
+    name = StringField('Name')
     external = BooleanField('External Experiment')
-    expName = TextField('expName')
-    expVersion = TextField('expVersion')
+    expName = StringField('expName')
+    expVersion = StringField('expVersion')
     active = BooleanField('Active')
     access_type = SelectField('Access Type', choices=[(x, x) for x in [u'public', u'password']])
-    password = TextField('Password')
+    password = StringField('Password')
     config = TextAreaField('Config')
     script = TextAreaField('Script')
     # TODO external / not external required fields
 
-class ExperimentExportForm(Form):
+class ExperimentExportForm(FlaskForm):
     format = SelectField('Format', choices=[(x,x) for x in [u'csv',
         u'excel_csv', u'json', u'excel']])
     replace_none = BooleanField('Replace none values\nnot for json')
-    none_value = TextField('None value replacement')
+    none_value = StringField('None value replacement')
 
-class UploadForm(Form):
-    file = FileField('Your File', [Required()])
+class UploadForm(FlaskForm):
+    file = FileField('Your File', [DataRequired()])
 
-class FolderForm(Form):
-    folder = TextField('Foler Name', [Required()])
+class FolderForm(FlaskForm):
+    folder = StringField('Foler Name', [DataRequired()])
