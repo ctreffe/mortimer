8557b8aa77b74226b8981bd12139976b701089cf
Johannes Brachem <jobrachem@posteo.de>
2020-04-09 14:22:01 +0200

    Update __init__.py

--- mortimer/__init__.py
+++ mortimer/__init__.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 from flask_dropzone import Dropzone
 import pymongo
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 # the EnvironSetter sets enviroment variables for the current session
 # It is not included in the GitHub repository, because it contains sensitive
